37cedd87f780c673869c94fa43bb631be8b36982
unknown <ronaldmcorona@gmail.com>
2020-02-17 22:14:33 -0800

    added drag sort

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+'use strict'
 // const fs = require('fs')
 const f = x => (console.log(x),x)
 //try{
@@ -63,23 +64,52 @@ function createItem(entry) {
     list.items.push(item)
     User.save()
 }
-function showItem(item,x) {
+function showItem(item,id) {
     const listContainer = D('list-container')
-    const html = document.createElement('div')
-    html.onmouseover = _ => html.className = 'table-dark';
-    (html.onmouseout = _ => html.className = 'table-' + (x % 2 === 0 ? 'primary' : 'secondary'))()
-    html.style.borderRadius = '2px'
-    html.style.width = '100%'
-    html.style.font = '80px Arial black'
-    html.innerHTML = (x+1) + '. ' + item.title
-    html.onclick = _ => { 
+    const itemDiv = document.createElement('div')
+    itemDiv.onmouseover = _ => itemDiv.className = 'table-dark';
+    (itemDiv.onmouseout = _ => itemDiv.className = 'table-' + (id % 2 === 0 ? 'primary' : 'secondary'))()
+    itemDiv.style.borderRadius = '2px'
+    itemDiv.style.width = '100%'
+    itemDiv.style.fontSize = '40px'
+    itemDiv.id = id
+    itemDiv.dataset.title = item.title
+    itemDiv.innerHTML = (id+1) + '. ' + item.title
+    itemDiv.ondragenter = function(e) {
+        // console.log('e =',e)
+        // console.log('toElem =',e.toElement)
+        // console.log('target =',e.target)
+        // console.log('srcElem =',e.srcElement)
+        this.style.borderBottom = '2px solid green'
+    }
+    itemDiv.ondragleave = function() {
+        this.style.border = ''
+    }
+    itemDiv.ondragend = function(e) {
+        const [y,x] = [e.clientY,e.clientX]
+        const arr = [...listContainer.children]
+        const indexDraggedInto = 
+            (arr.find(itm =>
+                getOffset(itm).top + this.scrollHeight > y
+            ) 
+            || arr[arr.length-1]).id
+
+        const children = [...listContainer.children]
+        listContainer.innerHTML = ''
+        list.items.length = 0
+        const elem = children.splice(id, 1)[0]
+        children.splice(indexDraggedInto, 0, elem)
+        children.forEach(c => createItem(c.dataset.title))
+    }
+    itemDiv.draggable = true
+    itemDiv.onclick = _ => { 
         const i = list.items.indexOf(item)
         User.path.push(i)
         list = list.items[i]
         loadList()
         User.save()
     }
-    listContainer.appendChild(html)
+    listContainer.appendChild(itemDiv)
 }
 
 D('entry').oninput = function() {
@@ -114,3 +144,26 @@ function Item(text) {
         items: []
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+function getOffset( el ) {
+    var _x = 0;
+    var _y = 0;
+    while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
+        _x += el.offsetLeft - el.scrollLeft;
+        _y += el.offsetTop - el.scrollTop;
+        el = el.offsetParent;
+    }
+    return { top: _y, left: _x };
+}
+var x = getOffset( document.getElementById('yourElId') ).left;
